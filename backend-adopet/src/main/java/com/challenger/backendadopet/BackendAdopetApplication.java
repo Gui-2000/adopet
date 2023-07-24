32f38a68e5b802c8ff3e2f0de4b12615dd7d6413
Guilherme Da Costa Santos <gdcsantos2000@gmail.com>
2023-07-24 16:27:40 -0300

    Backend project created

---
+++ backend-adopet/src/main/java/com/challenger/backendadopet/BackendAdopetApplication.java
@@ -0,0 +1,13 @@
+package com.challenger.backendadopet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAdopetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAdopetApplication.class, args);
+	}
+
+}
---
+++ backend-adopet/src/test/java/com/challenger/backendadopet/BackendAdopetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenger.backendadopet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAdopetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
