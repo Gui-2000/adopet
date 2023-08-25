8a7c8986613f97020e2dec6813d4ec1c11e84344
Guilherme Da Costa Santos <gdcsantos2000@gmail.com>
2023-08-25 15:27:52 -0300

    Validations entity Pet

--- backend-adopet/src/main/java/com/challenger/backendadopet/dtos/requesties/PetRequest.java
+++ backend-adopet/src/main/java/com/challenger/backendadopet/dtos/requesties/PetRequest.java
@@ -2,6 +2,9 @@ package com.challenger.backendadopet.dtos.requesties;
 
 import com.challenger.backendadopet.entities.Pet;
 import com.challenger.backendadopet.enums.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -9,17 +12,30 @@ import java.util.Objects;
 public class PetRequest {
 
     private Long id;
+    @Size(min = 3, max = 50, message = "Deve ter entre 3 e 50 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String name;
+    @NotBlank(message = "Campo requerido")
     private String age;
+    @NotNull(message = "Campo requerido")
     private String description;
+    @NotNull(message = "Campo requerido")
     private PetSpecie specie;
+    @NotNull(message = "Campo requerido")
     private PetPersonality personality;
+    @NotNull(message = "Campo requerido")
     private LocalDateTime dateCreate;
+    @NotNull(message = "Campo requerido")
     private PetAgeMonthOrYear ageMonthOrYear;
+    @NotNull(message = "Campo requerido")
     private PetGenre genre;
+    @NotNull(message = "Campo requerido")
     private PetStatus status;
+    @NotNull(message = "Campo requerido")
     private PetCarrying carrying;
+    @NotNull(message = "Campo requerido")
     private Long ownerId;
+    @NotNull(message = "Campo requerido")
     private Long shelterId;
 
     public PetRequest() {
